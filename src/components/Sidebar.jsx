03024650c20f9468639945ac1c56ddfdfd5d23c8
Eli Guy <eliguy343@gmail.com>
2023-01-02 16:59:49 +0200

    sidebar outer design

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,8 +18,86 @@ import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 
 const Sidebar = () => {
+
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("dashboard");
+  console.log(colors)
   return (
-    <div>Sidebar</div>
+    <Box
+      sx={{
+        "& .pro-sidebar-inner":{
+          background:`${colors.primary[400]} !important`
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: "transparent !important"
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px !important"
+        },
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important"
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important"
+        }
+      }}
+    >
+      <ProSidebar collapsed={isCollapsed}>
+        <Menu iconShape="square">
+          {/* LOGO AND MENU ICON */}
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Typography variant="h3" color={colors.grey[100]}>
+                  ADMINIS
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+            {!isCollapsed && (
+              <Box>
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <img
+                    alt ="logo"
+                    width="100px"
+                    height="100px"
+                    src={`../../assets/signal.png`}
+                    style={{ cursor: "pointer", borderRadius:"50%"}}
+                  />
+                </Box>
+                <Box textAlign="center">
+                  <Typography variant="h5"
+                    color={colors.grey[100]}
+                    fontWeight="bold"
+                    sx={{
+                      m: "10px 0 0 0"
+                    }}
+                  >
+                    Admin Dashboard
+                  </Typography>
+                </Box>
+              </Box>
+            )}
+          </MenuItem>
+        </Menu>
+      </ProSidebar>
+    </Box>
   )
 }
 
