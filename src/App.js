d58239873ddef7850acec41460e59eb59bd45cea
Eli Guy <eliguy343@gmail.com>
2023-01-02 08:50:45 +0200

    theme settings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="app">
-      Test
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <div className="app"></div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
