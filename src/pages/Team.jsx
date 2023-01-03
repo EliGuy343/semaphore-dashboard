36b26346dba15271e0c37492b8f86bd2384a2ee1
Eli Guy <eliguy343@gmail.com>
2023-01-03 09:53:15 +0200

    Column fix

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -11,7 +11,6 @@ const Team = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const columns = [
-    {field: "id", HeaderName: "ID"},
     { field: "id", headerName: "ID" },
     {
       field: "name",
