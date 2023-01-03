0bf09cd5bc57f12c728bc14471b27c342fe4a365
Eli Guy <eliguy343@gmail.com>
2023-01-03 08:58:26 +0200

    Dashboard Top

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,18 @@
+import { Box } from '@mui/system'
 import React from 'react'
+import Header from '../components/Header'
 
 const Dashboard = () => {
   return (
-    <div></div>
+    <Box m="20px">
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        align-items="center"
+      >
+        <Header title="DASHBOARD"  subtitle="welcome to your dashboard"/>
+      </Box>
+    </Box>
   )
 }
 
